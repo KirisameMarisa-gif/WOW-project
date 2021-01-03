03f18691960ee36d44fad3dadb928abb4d567b16
KirisameMarisa-gif <67214165+KirisameMarisa-gif@users.noreply.github.com>
2021-01-03 21:46:00 +0800

    yes

--- WOWY1jsscript.js
+++ WOWY1jsscript.js
@@ -31,11 +31,12 @@ $('td').click(function() {
             $(this).addClass('selected');
         }
         $('td').removeClass('wasRemoved');
+        if($(this).hasClass('unabletoactivate') == true) {
+            $(this).removeClass('activated');
+            $(this).removeClass('selected');
+        }
     }
 });
-function buttonClicked() {
-    document.getElementById("timetable").style.visibility = "visible";
-}
 function openForm() {
     document.getElementById("newEventForm").style.visibility = "visible";
     document.getElementById("submission").style.visibility = "visible";
@@ -48,13 +49,23 @@ $("#submission").click(function() {
     $(".activated").children("div.name").text(name);
     $(".activated").children("div.details").text(details);
     $(".activated").css("background-color", colour);
-    if (colour == "black") {
-        $(".activated").css("color", "white");
+    $(".activated").css("color", "white")
+    if (colour == "cyan") {
+        $(".activated").css("color", "black");
+    }
+    if (colour == "yellow") {
+        $(".activated").css("color", "black");
+    }
+    if (colour == "white") {
+        $(".activated").css("color", "black");
     }
     document.getElementById("newEventForm").style.visibility
     $("#inputName").val("");
     $("#inputDetails").val("");
+    $(".activated").addClass("unabletoactivate");
+    $(".selected").removeClass("selected");
     $(".activated").removeClass("activated");
     document.getElementById("newEventForm").style.visibility = "hidden";
     document.getElementById("submission").style.visibility = "hidden";
 });
+
